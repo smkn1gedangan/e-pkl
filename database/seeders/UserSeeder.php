0d1a3a7df2fe9ecc6e358f82ab3a7307dfd31a2f
hajir1 <paksutrisno934@gmail.com>
2025-06-06 21:23:36 +0700

    UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,96 +17,12 @@ class UserSeeder extends Seeder
         $datas = [
             [
             "name"=>"admin",
-            "email"=>"admin@gmail.com",
+            "email"=>"example@gmail.com",
             "email_verified_at"=>now(),
-            "password"=>"admin",
+            "password"=>env("SEED_ADMIN_PASSWORD","password"),
             "remember_token"=>Str::random(10),   
             "role"=>"admin",
             "kontak"=>"0123457890"
-            ],
-            [
-            "name"=>"pembimbing",
-            "email"=>"pembimbing@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"pembimbing",
-            "remember_token"=>Str::random(10),   
-            "role"=>"pembimbing_sekolah",
-            "kontak"=>"0123457890"
-
-            ],
-            [
-            "name"=>"pembimbing1",
-            "email"=>"pembimbing1@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"pembimbing1",
-            "remember_token"=>Str::random(10),   
-            "role"=>"pembimbing_pt",
-            "kontak"=>"0123457890"
-            ],
-            [
-            "name"=>"pembimbing2",
-            "email"=>"pembimbing2@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"pembimbing2",
-            "remember_token"=>Str::random(10),   
-            "role"=>"pembimbing_pt",
-            "kontak"=>"0123457890"
-
-            ],
-            [
-            "name"=>"siswa",
-            "email"=>"siswa10@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"siswa",
-            "remember_token"=>Str::random(10),   
-            "role"=>"siswa",
-            "kontak"=>"0123457890",
-            "isAccept"=>true
-            ],
-            [
-            "name"=>"siswa1",
-            "email"=>"siswa1@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"siswa1",
-            "remember_token"=>Str::random(10),   
-            "role"=>"siswa",
-            "kontak"=>"0123457890"
-            ],
-            [
-            "name"=>"siswa2",
-            "email"=>"siswa2@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"siswa",
-            "remember_token"=>Str::random(10),   
-            "role"=>"siswa",
-            "kontak"=>"0123457890",
-            ],
-            [
-            "name"=>"siswa3",
-            "email"=>"siswa3@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"siswa1",
-            "remember_token"=>Str::random(10),   
-            "role"=>"siswa",
-            "kontak"=>"0233457890"
-            ],
-            [
-            "name"=>"siswa4",
-            "email"=>"siswa4@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"siswa",
-            "remember_token"=>Str::random(10),   
-            "role"=>"siswa",
-            "kontak"=>"0983457890",
-            ],
-            [
-            "name"=>"siswa5",
-            "email"=>"siswa5@gmail.com",
-            "email_verified_at"=>now(),
-            "password"=>"siswa1",
-            "remember_token"=>Str::random(10),   
-            "role"=>"siswa",
-            "kontak"=>"5433457890"
             ],
         ];
         foreach ($datas as $data) {
