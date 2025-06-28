e2aa00c3a684efeb67bef72967607cf92977ef1f
hajir1 <paksutrisno934@gmail.com>
2025-06-28 19:11:49 +0700

    rename column isActive in laporans

--- database/migrations/2025_06_23_063325_create_laporans_table.php
+++ database/migrations/2025_06_23_063325_create_laporans_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('laporans', function (Blueprint $table) {
             $table->id();
             $table->string("judul")->unique();
-            $table->enum("status",["pending","tolak","terima"])->default("pending");
+            $table->enum("isActive",["pending","tolak","terima"])->default("pending");
             $table->foreignId("user_id")->nullable()->constrained()->onDelete("cascade");
             $table->timestamps();
         });
